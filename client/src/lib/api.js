4d029e08615ccfb2c8d4d7e1149996e05f5544ef
unknown <gajjar2022@gmail.com>
2025-11-29 10:42:43 +0530

    Api changed

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE || "https://expense01.vercel.app/",
+  baseURL: import.meta.env.VITE_API_BASE || "https://expense01.onrender.com",
 })
 
 // ✅ define setToken first
